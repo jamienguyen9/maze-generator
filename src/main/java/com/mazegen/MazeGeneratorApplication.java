c33d8c342e45bb4b2b6c78174424120562e4937a
Jamie Nguyen <jnguyen.software@gmail.com>
2025-06-03 22:52:26 -0400

    init

---
+++ src/main/java/com/mazegen/MazeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.mazegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazegen/MazeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
